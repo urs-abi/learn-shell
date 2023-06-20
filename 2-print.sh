5fe27af7dbf5f24ea930551b8daa1299cbfc6fd0
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-20 23:34:45 +0530

    colors

--- 2-print.sh
+++ 2-print.sh
@@ -1,1 +1,8 @@
 echo Hello Abhishek
+
+echo -e "\e[31mHello Abhishek in Red\e[0m"
+echo -e "\e[32mHello Abhishek in Green\e[0m"
+echo -e "\e[33mHello Abhishek in Yellow\e[0m"
+echo -e "\e[34mHello Abhishek in Blue\e[0m"
+echo -e "\e[35mHello Abhishek in Magenta\e[0m"
+echo -e "\e[36mHello Abhishek in Cyan\e[0m"
