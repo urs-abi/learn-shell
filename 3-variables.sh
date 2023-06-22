c86217bff5dd0bec34ab3a62b51ad69770ac3e27
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-23 02:08:37 +0530

    learn shell

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+a=10
+
+echo Value of a is $a
+    # Value of a is 10
+
+echo Value of a is $adollars
+    # Value of a is            -> Error
+
+echo Value of a is {a}dollars
+    # Value of a is 10dollars 
