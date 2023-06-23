04bbeff70b92924cab68506f2b1d6e9109a8c3a9
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-23 16:44:13 +0530

    functions

--- 4-functions.sh
+++ 4-functions.sh
@@ -5,3 +5,18 @@ print_hello() {
 print_hello
 
     Output - Hello World
+
+print_arguments() {
+  echo First Argument $1
+  echo Second Argument $2
+  echo All Arguments - $*
+  echo No of Argumetns - $#
+}
+
+print_arguments abc 123 xyz
+
+    Outout 
+    abc
+    123
+    abc 123 xyz
+    3
